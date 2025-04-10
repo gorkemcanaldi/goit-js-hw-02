11d61aca4115c8ad162ccbcc3938394e71f4d4a4
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-04-11 02:53:47 +0300

    task-3 tamam

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message) {
+  const yasaklıKelime1 = "spam";
+  const yasaklıKeline2 = "sale";
+  if (
+    message.toLowerCase().includes(yasaklıKelime1) ||
+    message.toLowerCase().includes(yasaklıKeline2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
